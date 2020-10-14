8da9d07957eeff74ee62977cfd1e692b391b6ec4
Michael A. Agarenzo <michaelagarenzo@mail.adelphi.edu>
2020-10-13 22:23:36 -0400

    Update spacing

--- src/changePage.js
+++ src/changePage.js
@@ -2,17 +2,13 @@
  * Change page without reloading site
  * @param {Event} event 
  * @param {String} page 
- */
+ **/
 function changePage(event, page) {
-
     var sections = document.getElementsByClassName("section");
 
     for (var i = 0; i < sections.length; i++) {
-
         sections[i].style.display = "none";
-
     }
 
     document.getElementById(page).style.display = "block";
-
 }
